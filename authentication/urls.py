96fcb0f3a5bd1d097ff45aa6133c1c24646d7c58
Satendra <satendra.raj1241@gmail.com>
2020-05-06 12:55:14 +0530

    coustom login added

--- authentication/urls.py
+++ authentication/urls.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf.urls.static import static
 from django.conf import settings
-from .views import registerView
+from .views import * 
 
 urlpatterns = [
-    path('register/',registerView),
+    path('register/',registerView,name='register'), 
+    path('loginhere/',loginView,name='loginhere')
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,19 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect 
 from django.contrib.auth import login,logout,authenticate
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import auth,User
+from django.http import JsonResponse
+
 # Create your views here.
 
 
 def loginView(request):
-    pass
-
+    if request.method=='POST':
+        username = request.POST.get('Username')
+        password = request.POST.get('password')
+        print(username)
+        print(password)
+        user = auth.authenticate(username=username,password=password)
+        print(user)
+        if user is not None:
+            auth.login(request,user)
+            contex={
+                'message':'success'
+            } 
+            return JsonResponse(contex)
+        else:
+            contex={
+                'message':'failed'
+            }
+            return JsonResponse(contex)
+    else:
+        return render(request,'registration/login.html')
 def logoutView(request):
     pass
 
 def registerView(request):
     if request.method=='POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-    form = UserCreationForm()
-    return render(request,'registration/signup.html',{'form':form})
+        username = request.POST.get('Username')
+        password = request.POST.get('password')
+        fname = request.POST.get('first_name')
+        lname = request.POST.get('last_name')
+        user = User.objects.create_user(username=username,password=password,first_name=fname,last_name=lname)
+        user.save()
+        auth.login(request,user)
+        return render(request,'index.html')
+    else:
+        return render(request,'registration/signup.html')
